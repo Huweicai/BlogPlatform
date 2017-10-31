3a7c4d36815b4616604a5221e5eb2cd9f7c00e1d
Anonymous Hu <huweicai@outlook.com>
2017-10-31 22:33:57 +0800

    新增简单首页，收录各功能

---
+++ WebContent/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<div align="center">
+	<h1>功能链接</h1>
+	<a href="./personalprofile">个人资料</a><br>
+	<a href="./resources/frontpage/login.jsp">登录</a><br/>
+	<a href="./resources/frontpage/register.jsp">注册</a><br>
+	<a href="./tip">提示页面</a><br>
+	</div>
+	
+</body>
+</html>
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -21,80 +21,89 @@ import utils.Const;
 
 @Controller
 public class UserController {
-	//换行符号
-	public String enter =  System.getProperty("line.separator");
+	// 换行符号
+	public String enter = System.getProperty("line.separator");
 	// 用线程安全的HashMap保存发送给邮箱的随机验证码
 	public static Map<String, Integer> randomKeys = Collections.synchronizedMap(new HashMap<String, Integer>());
-	//邮件内容
-	String content = "LONERS 收到了您的重置密码请求。" + enter +
-			"如果要重置密码，请单击下面的链接，或者将其复制并粘贴到浏览器中：\n"+enter
-			+ "如果不想重置密码，请忽略此邮件，您的密码不会改变。如有任何疑问或顾虑，请联系 LONERS团队 获取支持。" +enter
-			+ utils.Const.domain + "/resetpassword?key=";
+	// 邮件内容
+	String content = "LONERS 收到了您的重置密码请求。" + enter + "如果要重置密码，请单击下面的链接，或者将其复制并粘贴到浏览器中：\n" + enter
+			+ "如果不想重置密码，请忽略此邮件，您的密码不会改变。如有任何疑问或顾虑，请联系 LONERS团队 获取支持。" + enter + utils.Const.domain
+			+ "/resetpassword?key=";
+
 	// 给指定邮箱发送验证码
 	public void sendKey(String email) throws Exception {
 		int key = (int) (1000000000 * Math.random());
-		//通过Spring获取实例
+		// 通过Spring获取实例
 		UserOp userop = (UserOp) Const.context.getBean("userop");
-		//获取用户ID
+		// 获取用户ID
 		String userID = userop.getIDByEamil(email);
-		content = "尊敬的用户 "+ userID + ",您好 ！"+ enter +content;
+		content = "尊敬的用户 " + userID + ",您好 ！" + enter + content;
 		randomKeys.put(userID, key);
-		utils.SendMailUtil.sendMail(email, content + key+  "&userID=" + userID, "重置密码");
+		utils.SendMailUtil.sendMail(email, content + key + "&userID=" + userID, "重置密码");
 	}
+
 	public static void main(String[] args) throws Exception {
 		UserController i = new UserController();
 		i.sendEmail("huweicai@outlook.com");
 	}
+
 	@RequestMapping("/sendemail")
 	public String sendEmail(@RequestParam("email") String eamil) throws Exception {
 		sendKey(eamil);
 		return "afterSendEmail";
 	}
+
+	// 更改密码页面
 	@RequestMapping("/resetpassword")
 	public String setPasswordAgain(HttpServletRequest req) {
 		String userID = req.getParameter("userID");
 		String key = req.getParameter("key");
-		if( userID==null || key ==null) {
+		if (userID == null || key == null) {
 			System.out.println("IDorKey为空");
 			return "resetPasswordFail";
-		}else if(randomKeys.get(userID)-Integer.valueOf(key)!=0){
-			System.out.println("userID="+randomKeys.get(userID) + "key="+key);
+		} else if (randomKeys.get(userID) - Integer.valueOf(key) != 0) {
+			System.out.println("userID=" + randomKeys.get(userID) + "key=" + key);
 			System.out.println((randomKeys.get(userID) - Integer.valueOf(key)));
 			return "resetPasswordFail";
-		}else {
+		} else {
 			return "resetpassword";
 		}
 	}
-	//在数据库中更新密码
+
+	// 在数据库中更新密码
 	@RequestMapping("/resetpasswordondb")
 	public String resetpasswordOnDB(HttpServletRequest req) throws Throwable {
 		System.out.println("被调用了！！！");
-		String password= req.getParameter("reNewPassword");
-		String userID= req.getParameter("userID");
-		System.out.println("获取到"+"password="+password+"userID+"+userID);
-		userID=userID.substring(7);
+		String password = req.getParameter("reNewPassword");
+		String userID = req.getParameter("userID");
+		System.out.println("获取到" + "password=" + password + "userID+" + userID);
+		userID = userID.substring(7);
 		UserOp userop = (UserOp) Const.context.getBean("userop");
 		userop.resetPassword(userID, password);
 		return "resetpassword";
 	}
-	//个人资料
+
+	// 个人资料页面
 	@RequestMapping("personalprofile")
-	public String goPersonalProfile() {
-		return "personalprofile";
+	public ModelAndView goPersonalProfile() {
+		ModelAndView mav = new ModelAndView();
+		return null;
 	}
+
+	// 更新个人资料
 	@RequestMapping("updateprofile")
 	public String updateProfile(HttpServletRequest req) {
-		 ApplicationContext context = new ClassPathXmlApplicationContext("configuration/beans.xml");
-		 UserOp userop =  (UserOp) context.getBean("userop");
-		 User user = userop.getUserByID(req.getParameter("userID"));
-		 user.setUserID(req.getParameter("userID"));
-		 user.setEmail(req.getParameter("email"));
-		 user.setSex(req.getParameter("sex"));
-		 user.setUsername(req.getParameter("username"));
-		 user.setBirth_day(req.getParameter("birthday"));
-		 user.setSelf_introduce(req.getParameter("self_introduce"));
-		 userop.updateUser(user);
-		 System.out.println("更新用户资料："+user);
+		ApplicationContext context = new ClassPathXmlApplicationContext("configuration/beans.xml");
+		UserOp userop = (UserOp) context.getBean("userop");
+		User user = userop.getUserByID(req.getParameter("userID"));
+		user.setUserID(req.getParameter("userID"));
+		user.setEmail(req.getParameter("email"));
+		user.setSex(req.getParameter("sex"));
+		user.setUsername(req.getParameter("username"));
+		user.setBirth_day(req.getParameter("birthday"));
+		user.setSelf_introduce(req.getParameter("self_introduce"));
+		userop.updateUser(user);
+		System.out.println("更新用户资料：" + user);
 		return "personalprofile";
 	}
 }
